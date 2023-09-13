bd0743ba3b6209f68e5c651f00b0659bcbc27814
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-09-13 10:36:52 +0200

    fix(header): content and style

---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+	return (
+		<header>
+			<p>MERLIN</p>
+			<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='none'>
+				<path stroke='#fff' d='M8 0v16M0 8h16M1 4l13.856 8M4 14.928l8-13.856m-8 0 8 13.856M1 12l13.856-8' />
+			</svg>
+			<p>©2023</p>
+		</header>
+	)
+}

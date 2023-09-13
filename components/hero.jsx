bb1a430788c644a32ad504cc71c405f23f4136c3
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-09-13 10:38:21 +0200

    feat(hero): added content and style

---
+++ components/hero.jsx
@@ -0,0 +1,19 @@
+export default function hero() {
+	return (
+		<section className='hero'>
+			<div>
+				<h1>
+					3D <span className='ft-alt-it'>Artist</span>. <br />
+					Creative <span className='ft-alt-it'>developer</span>.
+					<br />I <span className='ft-alt'>mix</span> <span className='ft-alt-it'>both</span> and make{' '}
+					<span className='ft-alt-it'>stuff</span>.
+					<br />
+					<div>
+						<span className='ft-alt'>Paris - </span>
+						<span className='ft-alt-it'>Bordeaux</span>
+					</div>
+				</h1>
+			</div>
+		</section>
+	)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,20 @@ import ReactDOM from 'react-dom/client'
 import React, { Suspense } from 'react'
 import App from './App'
 import '../styles/index.scss'
-import Header from '../components/header/header'
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Nav from '../components/nav'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Suspense fallback={null}>
 			<App />
 			<div>
-				<Header />
+				<div className='hero-container'>
+					<Header />
+					<Hero />
+					<Nav />
+				</div>
 			</div>
 		</Suspense>
 	</React.StrictMode>

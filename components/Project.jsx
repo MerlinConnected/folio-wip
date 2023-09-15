9494db12768f638b7d3fc4fa77dd064224c2a4b2
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-09-15 14:58:51 +0200

    feat(motion): scale on hover img

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,37 +1,40 @@
-import { AnimatePresence } from 'framer-motion'
 import { motion } from 'framer-motion'
 import { useState } from 'react'
 
 export default function Project({ project }) {
-	const [isHovered, setIsHovered] = useState(false)
+	const [show, setShow] = useState(false) // Initially, not shown
 
-	const handleMouseEnter = () => {
-		setIsHovered(true)
+	const snappy = { duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] }
+	const slower = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] }
+
+	const handleClick = () => {
+		setShow(!show)
 	}
 
-	const handleMouseLeave = () => {
-		setIsHovered(false)
-	}
 	return (
-		<div className='container'>
-			<div className='project'>
-				<div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-					<h2>{project.title1}</h2>
-					<AnimatePresence>
-						{isHovered && (
-							<motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
-								<img src={project.image} alt='project-image' />
-							</motion.div>
-						)}
-					</AnimatePresence>
-					<h2>{project.title2}</h2>
-				</div>
-				<div>
-					<p>{project.client}</p>
-					<p>{project.type}</p>
-					<p>{project.soft}</p>
+		<>
+			<div className='container'>
+				<div className='project'>
+					<div>
+						<h2>{project.title1}</h2>
+						<div className={show ? 'image-container active' : 'image-container'}>
+							<motion.img
+								whileHover={{ scale: 1.1 }}
+								transition={slower}
+								onClick={handleClick}
+								src={project.image}
+								alt='project-image'
+							/>
+						</div>
+						<h2>{project.title2}</h2>
+					</div>
+					<div>
+						<p>{project.client}</p>
+						<p>{project.type}</p>
+						<p>{project.soft}</p>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }

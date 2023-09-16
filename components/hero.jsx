0998050b4844ce014d727a5992802f601224ad42
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-09-16 18:19:17 +0200

    feat(animation): hero section

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+import { useInView, motion } from 'framer-motion'
 
 export default function hero() {
 	useEffect(() => {
@@ -13,20 +14,90 @@ export default function hero() {
 			window.removeEventListener('scroll', handleScroll)
 		}
 	}, [])
+
+	const snappy = { ease: [0.6, 0.01, -0.05, 0.9] }
+	const slower = { ease: [0.43, 0.13, 0.23, 0.96] }
+
+	const ref = useRef(null)
+
+	const isInView = useInView(ref, { once: false })
+
+	const container = {
+		hidden: { opacity: 0 },
+		visible: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.2
+			}
+		}
+	}
+
+	const child = {
+		hidden: { y: 100 },
+		visible: { y: 0 }
+	}
 	return (
 		<section className='hero'>
-			<div>
-				<h1>
-					3D <span className='ft-alt-it'>Artist</span>. <br />
-					Creative <span className='ft-alt-it'>developer</span>.
-					<br />I <span className='ft-alt'>mix</span> <span className='ft-alt-it'>both</span> and make{' '}
-					<span className='ft-alt-it'>stuff</span>.
-					<br />
-					<div>
-						Paris <span className='ft-alt'> &</span> <span className='ft-alt-it'> Bordeaux</span>
-					</div>
-				</h1>
-			</div>
+			<motion.div ref={ref} initial='hidden' animate={isInView ? 'visible' : 'hidden'} variants={container}>
+				<div style={{ overflow: 'hidden' }}>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }}>
+						<h1>3D</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }} className='ft-alt-it'>
+						<h1>
+							<span className='ft-alt-it'>Artist.</span>
+						</h1>
+					</motion.div>
+				</div>
+				<div style={{ overflow: 'hidden' }}>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }}>
+						<h1>Creative</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }} className='ft-alt-it'>
+						<h1>
+							<span className='ft-alt-it'>developer.</span>
+						</h1>
+					</motion.div>
+				</div>
+				<div style={{ overflow: 'hidden' }}>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }}>
+						<h1>I</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }} className='ft-alt'>
+						<h1>
+							<span className='ft-alt'>mix</span>
+						</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }} className='ft-alt-it'>
+						<h1>
+							<span className='ft-alt-it'>both</span>
+						</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }}>
+						<h1>and make</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }} className='ft-alt-it'>
+						<h1>
+							<span className='ft-alt-it'>stuff.</span>
+						</h1>
+					</motion.div>
+				</div>
+				<div style={{ overflow: 'hidden' }}>
+					<motion.div variants={child} transition={{ ...slower, duration: 1 }}>
+						<h1>Paris</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 0.8 }} className='ft-alt'>
+						<h1>
+							<span className='ft-alt'>&</span>
+						</h1>
+					</motion.div>
+					<motion.div variants={child} transition={{ ...slower, duration: 1 }} className='ft-alt-it'>
+						<h1>
+							<span className='ft-alt-it'>Bordeaux</span>
+						</h1>
+					</motion.div>
+				</div>
+			</motion.div>
 		</section>
 	)
 }

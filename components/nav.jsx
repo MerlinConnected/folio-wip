f9724e2af29b65c7c2ea6a57f0aed79277e5de9f
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-09-18 09:03:15 +0200

    feat(animations): header and nav now animated

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import { motion } from 'framer-motion'
+
 export default function Nav() {
 	const githubLinks = ['G1THU8', 'G1THUB', 'GITHU8', 'G!THUB', 'G!THU8', 'GITHUß', 'G!THUß', 'G1THUß']
 	const linkedinLinks = ['L1NK3D1N', 'L1NKED1N', 'L!NK3D1N', 'L!NKED1N', 'L1NK3D!N', 'L!NK3D!N', 'L!NKED!N', 'L1NKED!N']
@@ -22,24 +24,36 @@ export default function Nav() {
 		setLinkedinText('-> LINKEDIN')
 	}
 
+	const slower = { ease: [0.43, 0.13, 0.23, 0.96] }
+
 	return (
 		<nav>
-			<a
-				href='https://github.com/MerlinConnected'
-				target='_blank'
-				onMouseEnter={handleGithubHover}
-				onMouseLeave={handleMouseLeave}
-			>
-				{githubText}
-			</a>
-			<a
-				href='https://www.linkedin.com/in/gaetan-jestin/'
-				target='_blank'
-				onMouseEnter={handleLinkedinHover}
-				onMouseLeave={handleMouseLeave}
-			>
-				{linkedinText}
-			</a>
+			<div style={{ overflow: 'hidden' }}>
+				<motion.a
+					initial={{ y: 100 }}
+					animate={{ y: 0 }}
+					transition={{ ...slower, duration: 0.6, delay: '0.8' }}
+					href='https://github.com/MerlinConnected'
+					target='_blank'
+					onMouseEnter={handleGithubHover}
+					onMouseLeave={handleMouseLeave}
+				>
+					{githubText}
+				</motion.a>
+			</div>
+			<div style={{ overflow: 'hidden' }}>
+				<motion.a
+					initial={{ y: 100 }}
+					animate={{ y: 0 }}
+					transition={{ ...slower, duration: 0.6, delay: '1' }}
+					href='https://www.linkedin.com/in/gaetan-jestin/'
+					target='_blank'
+					onMouseEnter={handleLinkedinHover}
+					onMouseLeave={handleMouseLeave}
+				>
+					{linkedinText}
+				</motion.a>
+			</div>
 		</nav>
 	)
 }

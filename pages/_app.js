33b3649ac819f21e239f278d5f6ab2083ca05492
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-04-25 11:58:29 +0200

    feat(App): icons & stuff

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,17 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<link rel='icon' href='/favicon.png' />
+				<link rel='icon' type='image/png' href='/images/icons/favicon-16x16.png' sizes='16x16' />
+				<link rel='icon' type='image/png' href='/images/icons/favicon-32x32.png' sizes='32x32' />
+				<link rel='apple-touch-icon' href='/images/icons/apple-touch-icon.png' />
+				<link
+					rel='icon'
+					type='image/png'
+					href='/images/icons/android-chrome-192x192.png'
+					sizes='192x192'
+				/>
+				<meta name='msapplication-TileColor' content='#ffffff' />
+				<meta name='msapplication-TileImage' content='/images/icons/mstile-144x144.png' />
 			</Head>
 
 			<Layout>

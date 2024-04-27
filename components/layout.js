f7f3543a4da0a825b0010e474e37310257e2b324
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-04-27 23:25:30 +0200

    build: added speedinsights

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import React, { Suspense } from 'react'
 import { ReactLenis } from '@studio-freight/react-lenis'
 
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function Layout({ children }) {
 	return (
@@ -13,6 +14,7 @@ export default function Layout({ children }) {
 				</div>
 			</Suspense>
 			<Analytics />
+			<SpeedInsights />
 		</ReactLenis>
 	)
 }

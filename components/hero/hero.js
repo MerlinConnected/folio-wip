7ab5189929e8be32bacf15197ffc61649d8f16b9
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-04-27 23:11:59 +0200

    feat(Hero): hero animation

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,11 +1,37 @@
-import { useEffect } from 'react'
-import { state } from '@/utils/store'
+import { useEffect, useRef } from 'react'
+
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap/dist/gsap'
+import { SplitText } from 'gsap/dist/SplitText'
+
+gsap.registerPlugin(useGSAP, SplitText)
 
 export default function Hero() {
+	const titleSectionRef = useRef()
+	const titleRef = useRef()
+
+	useGSAP(() => {
+		const split = new SplitText(titleRef.current, { type: 'lines' })
+		gsap.from(split.lines, {
+			yPercent: 100,
+			opacity: 0,
+			delay: 3.3,
+			stagger: 0.1,
+			duration: 1,
+			ease: 'power3.out'
+		})
+
+		gsap.to(titleSectionRef.current, {
+			y: 0,
+			delay: 3.2,
+			duration: 2,
+			ease: 'expo.out'
+		})
+	})
+
 	useEffect(() => {
 		const handleScroll = () => {
-			const title = document.querySelector('.hero')
-			title.style.transform = `translateY(-${window.scrollY / 2}px)`
+			titleSectionRef.current.style.transform = `translateY(-${window.scrollY / 2}px)`
 		}
 
 		window.addEventListener('scroll', handleScroll)
@@ -16,14 +42,10 @@ export default function Hero() {
 	}, [])
 
 	return (
-		<section className='hero'>
-			<h1>
-				<span>3D Artist.</span>
-				<span>Creative developer.</span>
-				<span>I mix both and make stuff.</span>
-				<span>
-					<span>Bordeaux</span> & <span>Paris</span>
-				</span>
+		<section ref={titleSectionRef} className='hero'>
+			<h1 ref={titleRef}>
+				3D Artist. <br /> Creative developer. <br /> I mix both and do artsy stuff. <br /> Bordeaux
+				& Paris
 			</h1>
 		</section>
 	)

8731dff022e88a0dedc99fc66b747e8a4e0e84b1
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-05-01 20:37:03 +0200

    feat(Projects): updated layouts

--- components/content/Project.jsx
+++ components/content/Project.jsx
@@ -1,30 +1,37 @@
+import Image from 'next/image'
+import { useAppContext } from '../context/ContextProvider'
+
 export default function Project({ project }) {
+	const { isMobile } = useAppContext()
+	console.log(isMobile)
 	return (
 		<>
 			<div className='container'>
 				<div className='project'>
-					<div>
+					<div className='project_title'>
 						<div>
 							<h2>{project.title1}</h2>
 						</div>
-						<div className='image-container'>
-							<img src={project.image} alt='project-image' />
-						</div>
+						{!isMobile && (
+							<div className='image-container'>
+								<Image
+									src={project.image}
+									alt='project-image'
+									width={1920}
+									height={1080}
+									quality={70}
+								/>
+							</div>
+						)}
 						<div>
 							<h2>{project.title2}</h2>
 						</div>
 					</div>
-					<div>
-						<div>
-							<p>{project.client}</p>
-						</div>
-						<div>
-							<p>{project.type}</p>
-						</div>
-						<div>
-							<p>{project.soft}</p>
-						</div>
-					</div>
+					{/* <div className='project_description'>
+						<p>{project.client}</p>
+						<p>{project.type}</p>
+						<p>{project.soft}</p>
+					</div> */}
 				</div>
 			</div>
 		</>

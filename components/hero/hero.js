6eaaa415b2d34e8195ba904cce70c7d828eab798
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-05-01 20:36:42 +0200

    fix(Hero): animation only on desktop

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react'
 
+import { useAppContext } from '../context/ContextProvider'
+
 import { useGSAP } from '@gsap/react'
 import { gsap } from 'gsap/dist/gsap'
 import { SplitText } from 'gsap/dist/SplitText'
@@ -7,6 +9,8 @@ import { SplitText } from 'gsap/dist/SplitText'
 gsap.registerPlugin(useGSAP, SplitText)
 
 export default function Hero() {
+	const { isMobile } = useAppContext()
+
 	const titleSectionRef = useRef()
 	const titleRef = useRef()
 
@@ -29,41 +33,42 @@ export default function Hero() {
 		})
 	})
 
-	useEffect(() => {
-		if (titleRef.current) {
-			const split = new SplitText(titleRef.current, { type: 'chars' })
-			split.chars.forEach((char) => {
-				gsap.set(char, { '--weight': '74' })
-				char.addEventListener('mouseenter', handleHover.bind(null, char, split.chars))
-				char.addEventListener('mouseleave', handleMouseLeave.bind(null, char, split.chars))
+	if (!isMobile) {
+		useEffect(() => {
+			if (titleRef.current) {
+				const split = new SplitText(titleRef.current, { type: 'chars' })
+				split.chars.forEach((char) => {
+					gsap.set(char, { '--weight': '74' })
+					char.addEventListener('mouseenter', handleHover.bind(null, char, split.chars))
+					char.addEventListener('mouseleave', handleMouseLeave.bind(null, char, split.chars))
 
-				return () => {
-					char.removeEventListener('mouseenter', handleHover)
-					char.removeEventListener('mouseleave', handleMouseLeave)
-				}
+					return () => {
+						char.removeEventListener('mouseenter', handleHover)
+						char.removeEventListener('mouseleave', handleMouseLeave)
+					}
+				})
+			}
+		}, [])
+
+		const handleHover = (target, chars) => {
+			const targetIndex = chars.indexOf(target)
+			const strength = 2
+
+			chars.forEach((char, index) => {
+				const distance = Math.abs(index - targetIndex)
+				const scaleFactor = 1 / (distance + 1)
+				const newWeight = 74 + (218 - 74) * scaleFactor * strength
+
+				gsap.to(char, { '--weight': newWeight, duration: 0.2 })
 			})
 		}
-	}, [])
 
-	const handleHover = (target, chars) => {
-		const targetIndex = chars.indexOf(target)
-		const strength = 2
-
-		chars.forEach((char, index) => {
-			const distance = Math.abs(index - targetIndex)
-			const scaleFactor = 1 / (distance + 1)
-			const newWeight = 74 + (218 - 74) * scaleFactor * strength
-
-			gsap.to(char, { '--weight': newWeight, duration: 0.2 })
-		})
+		const handleMouseLeave = (target, chars) => {
+			chars.forEach((char) => {
+				gsap.to(char, { '--weight': '74', duration: 0.2 })
+			})
+		}
 	}
-
-	const handleMouseLeave = (target, chars) => {
-		chars.forEach((char) => {
-			gsap.to(char, { '--weight': '74', duration: 0.2 })
-		})
-	}
-
 	useEffect(() => {
 		const handleScroll = () => {
 			titleSectionRef.current.style.transform = `translateY(-${window.scrollY / 2}px)`

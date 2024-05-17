2c1a56edb5cf9301368f0a55d8e98f2fdeed9abf
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-05-17 22:32:36 +0200

    refactor(ProjectsLayout): projects are now dynamically added to the page

--- components/content/ProjectsLayout.jsx
+++ components/content/ProjectsLayout.jsx
@@ -1,18 +1,19 @@
 import Project from './Project'
 
-import { photorealSubway, hangingLights } from '@/utils/projects'
+import { projects } from '@/utils/projects'
 
 export default function Projects() {
 	return (
 		<section className='projects'>
 			<div>
 				<p>
-					Projects<sup>2</sup>
+					Projects<sup>{projects.length}</sup>
 				</p>
 			</div>
 			<div>
-				<Project project={hangingLights} />
-				<Project project={photorealSubway} />
+				{projects.map((project, index) => (
+					<Project project={project} key={index} />
+				))}
 			</div>
 		</section>
 	)

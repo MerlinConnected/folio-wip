637e4315a0a18fef6bca36e2b8a5927e2d1b1204
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-05-17 22:31:28 +0200

    feat(projects): updated projects

--- utils/projects.js
+++ utils/projects.js
@@ -1,15 +1,28 @@
-export const hangingLights = {
+const hangingLights = {
 	title1: 'Hanging',
-	title2: 'Lights',
+	title2: 'Bulbs',
 	image: '/images/hanging-lights.png',
 	year: '2021',
 	client: 'Personal Project',
 	type: '3D',
 	soft: 'Blender',
-	pageUrl: '/hanging-lights'
+	pageUrl: '/hanging-lights',
+	newTab: false
 }
 
-export const photorealSubway = {
+const helmetConfigurator = {
+	title1: 'Helmet',
+	title2: 'Configurator',
+	image: '/images/helmet-configurator.png',
+	year: '2023',
+	client: 'Personal Project',
+	type: 'R3F',
+	soft: 'Substance Painter',
+	pageUrl: 'https://helmet-configurator.vercel.app',
+	newTab: true
+}
+
+const photorealSubway = {
 	title1: 'Photoreal',
 	title2: 'Subway',
 	image: '/images/photoreal-subway.png',
@@ -17,5 +30,20 @@ export const photorealSubway = {
 	client: 'Personal Project',
 	type: '3D',
 	soft: 'Blender',
-	pageUrl: '/photoreal-subway'
+	pageUrl: '/photoreal-subway',
+	newTab: false
 }
+
+const roofTiles = {
+	title1: 'Roof',
+	title2: 'Tiles',
+	image: '/images/roof-tiles.jpg',
+	year: '2021',
+	client: 'Personal Project',
+	type: '3D',
+	soft: 'Blender',
+	pageUrl: '/roof-tiles',
+	newTab: false
+}
+
+export const projects = [hangingLights, helmetConfigurator, photorealSubway, roofTiles]
